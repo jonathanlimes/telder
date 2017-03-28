81b754084cde63c0b5a96f4366f57bd4731523f3
Jonathan Lim <jonathanlimes@gmail.com>
2017-03-28 22:01:35 +0800

    added static front page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'notes#index'
+  root 'static#home'
   get 'tagged' => 'notes#tagged', :as => 'tagged'
 end
