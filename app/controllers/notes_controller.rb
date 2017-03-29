6fdb54c672bf34efa4f1aad07c62697d02bae14e
Jonathan Lim <jonathanlimes@gmail.com>
2017-03-29 22:19:15 +0800

    routing fixed for join

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -84,6 +84,11 @@ class NotesController < ApplicationController
     end
   end
 
+  def join
+    # fix this
+    # current_user.notes << @note
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
   get 'tagged' => 'notes#tagged', :as => 'tagged'
+  get 'join' => 'notes#join', :as => 'join'
 end
