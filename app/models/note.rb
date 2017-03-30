2d5fea4cdcdcee99fef027de50ae65e911e48ee6
Jonathan Lim <jonathanlimes@gmail.com>
2017-03-30 13:09:56 +0800

    validation error fixed

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ApplicationRecord
 
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, class_name: "User", optional: true
   has_and_belongs_to_many :users
 
   acts_as_taggable_on :tags
