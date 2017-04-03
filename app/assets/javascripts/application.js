ab1e840071c6ae57061d5f9b785266a52008846a
Jonathan Lim <jonathanlimes@gmail.com>
2017-04-03 18:23:29 +0800

    caret function solved

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.caret
 //= require turbolinks
 //= require_tree .
 //= require_tree ./channels
--- app/assets/javascripts/channels/notes.js
+++ app/assets/javascripts/channels/notes.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function () {
   connectRealTime()
 })
 
@@ -9,25 +9,30 @@ function connectRealTime () {
     note_id: current_note_id },
 
     {
-    received: function (data) {
-      console.log('received input')
-      // captures position of cursor
-      // replace previous content with new Content
-      $('#note-text').val(data.text)
-    },
-    connected: function () {
-      $('#text-label').removeClass("is-dark").addClass("is-primary")
-      $('#text-label').text("Connected to Server")
-      console.log('Client-side channel connected to server')
-    },
-    disconnected: function () {
-      $('#text-label').removeClass("is-dark").addClass("is-danger")
-      $('#text-label').text("Disconnected from Server")
-      console.log('Client-side channel disconnected from server')
-    },
-    rejected: function () {
-      console.log('rejected')
+      received: function (data) {
+        console.log('received input')
+        // capture last known position of cursor
+        var lastCursorPosition = $('#note-text').caret()
+        console.log(lastCursorPosition)
+        // replace previous content with new Content
+        $('#note-text').val(data.text)
+        // return cursor position to last known position
+        $('#note-text').caret(lastCursorPosition, 1)
+        console.log('restored last cursor position')
+      },
+      connected: function () {
+        $('#text-label').addClass('is-primary')
+        $('#text-label').text('Connected to Server')
+        console.log('Client-side channel connected to server')
+      },
+      disconnected: function () {
+        $('#text-label').addClass('is-danger')
+        $('#text-label').text('Disconnected from Server')
+        console.log('Client-side channel disconnected from server')
+      },
+      rejected: function () {
+        console.log('rejected')
+      }
     }
-  }
 )
 }
